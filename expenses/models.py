b4104638a73c69f0f338536d55553f957384a532
KeoCode <keocode101@gmail.com>
2023-03-03 15:01:44 +0000

    Edit styling of Footer and header and migrated database updates

--- expenses/models.py
+++ expenses/models.py
@@ -1,15 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
+from datetime import datetime, date
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 STATUS = ((0, "Draft"), (1, "Published"))
 
 
 class Post(models.Model):
-    title = models.CharField(max_length=200, unique=True)
+    title = models.CharField(max_length=200, unique=True, null=False, blank=False)
     slug = models.SlugField(max_length=200, unique=True)
+    date = models.DateField(("Date"), default=date.today)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="expense_post")
     udated_on = models.DateTimeField(auto_now=True)
+    expense_amount = models.PositiveIntegerField(default=0, validators=[MinValueValidator(1), MaxValueValidator(100000)])
     content = models.TextField()
     featured_image = CloudinaryField('image', default='placeholder')
     excerpt = models.TextField(blank=True)
